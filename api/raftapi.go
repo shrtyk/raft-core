86d5e409ae0ec935be3e1e062a273670e1557fc3
shortyk <shortykofficial@gmail.com>
2025-10-21 02:06:28 +0300

    feat: add api for raft

---
+++ api/raftapi.go
@@ -0,0 +1,31 @@
+package api
+
+// The Raft interface
+type Raft interface {
+	// Start agreement on a new log entry, and return the log index
+	// for that entry, the term, and whether the peer is the leader.
+	Start(command []byte) (int64, int64, bool)
+
+	// Ask a Raft for its current term, and whether it thinks it is
+	// leader
+	GetState() (int64, bool)
+
+	// For Snaphots
+	Snapshot(index int64, snapshot []byte)
+	PersistBytes() int
+
+	// For the tester to indicate to your code that is should cleanup
+	// any long-running go routines.
+	Kill()
+}
+
+type ApplyMessage struct {
+	CommandValid  bool
+	Command       []byte
+	CommandIndex  int64
+
+	SnapshotValid bool
+	Snapshot      []byte
+	SnapshotIndex int64
+	SnapshotTerm  int64
+}
