1ab02d0e6e8593d9a0ad0917d7469ad038671c7c
shortyk <shortykofficial@gmail.com>
2025-10-25 20:21:26 +0300

    feat: added intial transport interface

---
+++ api/transport.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"context"
+
+	raftpb "github.com/shrtyk/raft-core/internal/proto/gen"
+)
+
+// Defines how Raft peers send RPCs to each other
+type Transport interface {
+	SendRequestVote(
+		ctx context.Context, to int, req *raftpb.RequestVoteRequest) (*raftpb.RequestVoteResponse, error)
+	SendAppendEntries(
+		ctx context.Context, to int, req *raftpb.AppendEntriesRequest) (*raftpb.AppendEntriesResponse, error)
+	SendInstallSnapshot(
+		ctx context.Context, to int, req *raftpb.InstallSnapshotRequest) (*raftpb.InstallSnapshotResponse, error)
+
+	// Stop closes all connections ir internal resources
+	Close() error
+}
