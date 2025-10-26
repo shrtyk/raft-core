0e1229dc11d6e6988232c50fc4a2c7a5a2390db1
shortyk <shortykofficial@gmail.com>
2025-10-26 19:27:54 +0300

    tests: added in-mem persister

---
+++ raft/tests/mem_persister.go
@@ -0,0 +1,44 @@
+package testsim
+
+import (
+	"github.com/shrtyk/raft-core/raft/tests/harness"
+)
+
+// MemPersister is an in-memory persister that implements the api.Persister interface.
+// It uses the harness.Persister for storage.
+type MemPersister struct {
+	p *harness.Persister
+}
+
+func NewMemPersister() *MemPersister {
+	return &MemPersister{
+		p: harness.MakePersister(),
+	}
+}
+
+func (ps *MemPersister) SaveRaftState(state []byte) error {
+	ps.p.Save(state, ps.p.ReadSnapshot())
+	return nil
+}
+
+func (ps *MemPersister) SaveSnapshot(snapshot []byte) error {
+	ps.p.Save(ps.p.ReadRaftState(), snapshot)
+	return nil
+}
+
+func (ps *MemPersister) SaveStateAndSnapshot(state, snapshot []byte) error {
+	ps.p.Save(state, snapshot)
+	return nil
+}
+
+func (ps *MemPersister) ReadRaftState() ([]byte, error) {
+	return ps.p.ReadRaftState(), nil
+}
+
+func (ps *MemPersister) ReadSnapshot() ([]byte, error) {
+	return ps.p.ReadSnapshot(), nil
+}
+
+func (ps *MemPersister) RaftStateSize() (int, error) {
+	return ps.p.RaftStateSize(), nil
+}
