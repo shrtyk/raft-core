862dd0ea91509c8d02853efb33e022aae90589ed
shortyk <shortykofficial@gmail.com>
2025-11-04 20:10:09 +0300

    feat: add builder interface

---
+++ api/builder.go
@@ -0,0 +1,23 @@
+package api
+
+import "log/slog"
+
+// NodeBuilder is an interface for constructing a Raft node.
+type NodeBuilder interface {
+	// Build constructs and returns a new Raft instance based on the
+	// configurations provided to the builder.
+	Build() Raft
+
+	// WithConfig sets the Raft configuration for the node.
+	// If not provided, a DefaultConfig will be used.
+	WithConfig(*RaftConfig) NodeBuilder
+
+	// WithPersister sets a custom Persister implementation for the node.
+	// If not provided, a DefaultStorage (filesystem-based) will be used.
+	WithPersister(Persister) NodeBuilder
+
+	// WithLogger sets a custom slog.Logger for the node.
+	// If not provided, a default logger based on the RaftConfig's Log.Env
+	// will be used.
+	WithLogger(*slog.Logger) NodeBuilder
+}
