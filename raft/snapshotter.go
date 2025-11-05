443f65ee02987f525ca23b58e8524d25d62fcb8c
shortyk <shortykofficial@gmail.com>
2025-11-05 19:40:27 +0300

    feat: add initial snapshotter implementation

---
+++ raft/snapshotter.go
@@ -0,0 +1,58 @@
+package raft
+
+import (
+	"log/slog"
+	"time"
+
+	"github.com/shrtyk/raft-core/pkg/logger"
+)
+
+// snapshotter is a background goroutine
+// that periodically checks if a snapshot needs to be taken.
+func (rf *Raft) snapshotter() {
+	defer func() {
+		rf.wg.Done()
+		rf.logger.Info("snapshotter exiting")
+	}()
+
+	rf.checkAndTakeSnapshot()
+
+	ticker := time.NewTicker(rf.cfg.Snapshots.CheckLogSizeInterval)
+	defer ticker.Stop()
+
+	rf.logger.Info("snapshotter starting")
+	for {
+		select {
+		case <-rf.raftCtx.Done():
+			return
+		case <-ticker.C:
+			rf.checkAndTakeSnapshot()
+		}
+	}
+}
+
+func (rf *Raft) checkAndTakeSnapshot() {
+	rf.mu.RLock()
+	size := rf.logSizeInBytes
+	lastApplied := rf.lastAppliedIdx
+	rf.mu.RUnlock()
+
+	if size < rf.cfg.Snapshots.ThresholdBytes {
+		return
+	}
+
+	rf.logger.Info(
+		"log size exceeds threshold, requesting snapshot from FSM",
+		slog.Int("size", size),
+		slog.Int("threshold", rf.cfg.Snapshots.ThresholdBytes))
+
+	snapshotData, err := rf.fsm.Snapshot()
+	if err != nil {
+		rf.logger.Warn("failed to get snapshot from FSM", logger.ErrAttr(err))
+		return
+	}
+
+	if err := rf.Snapshot(lastApplied, snapshotData); err != nil {
+		rf.logger.Warn("failed to apply snapshot", logger.ErrAttr(err))
+	}
+}
